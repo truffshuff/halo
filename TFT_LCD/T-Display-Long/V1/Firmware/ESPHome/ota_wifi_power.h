16fa562ed97f2ca106a17cea10760ae99ca31bc6
truffshuff <98434578+truffshuff@users.noreply.github.com>
2026-07-09 21:21:13 -0400

    Fix remote ESPHome OTA build

--- TFT_LCD/T-Display-Long/V1/Firmware/ESPHome/ota_wifi_power.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "esp_wifi.h"
-
-inline void halo_set_ota_wifi_power(bool enabled) {
-  // ESP-IDF uses quarter-dBm units: 60 = 15 dBm, 34 = 8.5 dBm.
-  esp_wifi_set_max_tx_power(enabled ? 60 : 34);
-}
